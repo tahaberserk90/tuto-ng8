c05a357818148b43bc98ad269ff0f805df43abc4
taha yahia radhi <tahaberserk@gmail.com>
2020-01-04 22:58:23 +0100

    2 step in shop app

--- shop-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shop-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,12 +9,20 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes : Recipe[]=[
-    new Recipe('recipe 1','this recipe 1','https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-11-recipe-easy-sheet-pan-nachos%2F2019-10-21_Kitchn89063_Easy-Sheet-Pan-Chicken-Nachos')
+    new Recipe('recipe 1','this recipe 1','https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-11-recipe-easy-sheet-pan-nachos%2F2019-10-21_Kitchn89063_Easy-Sheet-Pan-Chicken-Nachos'),
+    new Recipe('recipe 1','this recipe 2','https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_760,h_950/k%2FPhoto%2FRecipes%2F2019-11-recipe-easy-sheet-pan-nachos%2F2019-10-21_Kitchn89063_Easy-Sheet-Pan-Chicken-Nachos')
   ];
+
+  @Output()
+  recipeWasSelected=new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipeEl:Recipe){
+    this.recipeWasSelected.emit(recipeEl);
   }
 
 }
--- shop-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shop-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredinet.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput',{static:false})
+  nameInput:ElementRef;
+  @ViewChild('amountInput',{static:false})
+  amountInput:ElementRef;
+  @Output()
+  ingredientAdded=new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addItem(){
+    const name=this.nameInput.nativeElement.value;
+    const amount=this.amountInput.nativeElement.value;
+    const ingredient=new Ingredient(name,amount);
+    this.ingredientAdded.emit(ingredient);
   }
 
 }
